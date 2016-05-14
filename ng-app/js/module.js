8a3fe0126175a03dbeb635c7d2f163673fe0831b
Deemo <792229765@qq.com>
2016-05-14 17:31:23 +0800

    ng

---
+++ ng-app/js/module.js
@@ -0,0 +1,22 @@
+/*
+ * angular demo
+ */
+var myModule = angular.module('myModule', []);
+
+myModule.controller('myCtlr', function($scope) {
+	$scope.name = '萌萌';
+	$scope.data = [
+		{
+			job: '战士',
+			level: 15
+		},
+		{
+			job: '法师',
+			level: 16
+		},
+		{
+			job: '游侠',
+			level: 17
+		}
+	]
+});
