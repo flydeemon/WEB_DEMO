94d656b2eb49823b349b151e917d4dfb444296e0
Deemo <792229765@qq.com>
2016-06-01 19:45:59 +0800

    14指令例子

--- ng-app/12ng指令自定义/module.js
+++ ng-app/12ng指令自定义/module.js
@@ -9,10 +9,10 @@ var myModule = angular.module('myModule', []);
 });*/
 
 /*
- * A: 属性 	attribute
+ * A: 属性 	attribute 	一般用于功能性操作
  * C: 类		class
- * E: 元素	element	
- * M:注释	comment
+ * E: 元素	element		一般用于模板替换
+ * M: 注释	comment
  */
 myModule.directive('hello', function(){
 	return {
